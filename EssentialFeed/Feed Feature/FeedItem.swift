33bdd7f3b2864b444870c1babd3fb23f0a89ad35
aquila choi <ippal97@hanmail.net>
2024-10-02 22:04:44 +0900

    최초 커밋

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -1,31 +1,19 @@
 //
-//  FeedItem.swift
-//  EssentialFeed
-//
-//  Created by 450635 on 8/14/24.
+//  Copyright © 2018 Essential Developer. All rights reserved.
 //
 
 import Foundation
 
 public struct FeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
-
-extension FeedItem: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
+	public let id: UUID
+	public let description: String?
+	public let location: String?
+	public let imageURL: URL
+	
+	public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+		self.id = id
+		self.description = description
+		self.location = location
+		self.imageURL = imageURL
+	}
 }
--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -1,18 +1,14 @@
 //
-//  FeedLoader.swift
-//  EssentialFeed
-//
-//  Created by 450635 on 8/14/24.
+//  Copyright © 2018 Essential Developer. All rights reserved.
 //
 
 import Foundation
 
-enum LoadFeedResult {
-    case success([FeedItem])
-    case error(Error)
+public enum LoadFeedResult {
+	case success([FeedItem])
+	case failure(Error)
 }
 
-protocol FeedLoader {
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+public protocol FeedLoader {
+	func load(completion: @escaping (LoadFeedResult) -> Void)
 }
-
