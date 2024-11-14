ca80df4fb8559b54977f2a0a70690facdc823bda
HW-11135 <choisk@hanwha.com>
2024-11-14 20:16:01 +0900

    LocalFeedLoader 생성 시 캐시 삭제 하지 않음

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by 450635 on 11/14/24.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+}
