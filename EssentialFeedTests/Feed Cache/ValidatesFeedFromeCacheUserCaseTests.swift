60e3f178e8a36356222871cadb2612eebfe162b7
HW-11135 <choisk@hanwha.com>
2024-11-22 16:08:13 +0900

    deallocated 된 instance를 조회하는 오류 수정

--- EssentialFeedTests/Feed Cache/ValidatesFeedFromeCacheUserCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidatesFeedFromeCacheUserCaseTests.swift
@@ -70,6 +70,17 @@ class ValidatesFeedFromeCacheUserCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
+    func test_validate_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        sut?.validateCache()
+        
+        sut = nil
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
     
     // MARK: - Helpers
     
